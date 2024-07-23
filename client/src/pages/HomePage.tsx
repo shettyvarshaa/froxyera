a5a706885ba3ea56fd9b89cddbed5bba1a5b3bae
shettyvarshaa <shettyvarshaa275@gmail.com>
2024-07-24 02:36:43 +0530

    Added initial files

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import LazyNav from "../components/LazyNav"
+function HomePage() {
+  return (
+    <div>
+      <h1 className="text-amber-300">Hi !</h1>
+      <LazyNav page = 'home'/>
+    </div>
+  )
+}
+
+export default HomePage
