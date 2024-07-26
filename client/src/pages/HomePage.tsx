ccda59d84d980a9b6368be468e8ea1fb69dfcf6f
siliconvolley <rishabhlaxmikanth@gmail.com>
2024-07-27 03:04:19 +0530

    added homepage banner

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,17 +1,29 @@
 
-import LazyNav from "../components/LazyNav";
-import { ModeToggle } from "@/components/mode-toggle";
-import './HomePage.css'; // Assuming you are using CSS files for styling
-
 function HomePage() {
   return (
-    <div className="homepage-container">
-      <ModeToggle />
-      <div className="content">
-        <h1>Hi !</h1>
-        <LazyNav page='home' />
+    <>
+      <div id="homepage-container">
+        <section
+          id="homepage-banner"
+          className="grid place-items-center py-32"
+          style={{
+            backgroundImage: `url(https://static.wixstatic.com/media/9d7009_fbdad113070f405bbe1e6a5f33c4b3f5~mv2_d_6016_4016_s_4_2.jpg/v1/fill/w_1760,h_482,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/9d7009_fbdad113070f405bbe1e6a5f33c4b3f5~mv2_d_6016_4016_s_4_2.jpg)`,
+          }}
+        >
+          <div
+            id="banner-title"
+            className="max-w-[48rem] grid place-items-center text-center border-2 border-white text-white px-36 py-10"
+          >
+            <h1 className="uppercase font-extralight text-[3rem] tracking-[0.5em] leading-[4.5rem]">
+              FROXYERA
+            </h1>
+            <h2 className="uppercase font-light text-[1.25rem] tracking-[0.25em]">
+              Photographer
+            </h2>
+          </div>
+        </section>
       </div>
-    </div>
+    </>
   );
 }
 
