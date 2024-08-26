740b4db4a560f349431d1ac6c84de1a2e1b255cf
shettyvarshaa <shettyvarshaa275@gmail.com>
2024-08-27 03:13:46 +0530

    Added template files

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import {Navbar} from './components';
+
 const App = ()  => {
   return (
     <div>
       <div>
         <Navbar />
+        <portfolio />
       </div>
     </div>
   )
---
+++ client/src/pages/index.js
@@ -0,0 +1,5 @@
+import portfolio from './portfolio/portfolio'
+
+export {
+    portfolio,
+};
